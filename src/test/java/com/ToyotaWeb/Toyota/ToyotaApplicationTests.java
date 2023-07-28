b44f5478f3d3bd7dc1976e4835bf31433a33e533
Thanujana <janat-ec20030@stu.kln.ac.lk>
2023-07-28 12:00:00 +0530

    developing REST APIs for Toyota’s enterprise application

---
+++ src/test/java/com/ToyotaWeb/Toyota/ToyotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ToyotaWeb.Toyota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
